26d9a3775c72457f0d936f1fcd39429b3b4739aa
rudiruhl <rudigd82@gmail.com>
2021-09-05 07:36:43 +0200

    :rocket: Added postCSS with autoprefixer & cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
 // compile scc into css
 function style (){
   return gulp.src('src/scss/**/*.scss')
   .pipe(sass())
-  .pipe(gulp.dest('dist/css'))
+  .pipe(gulp.dest('src/css'))
   .pipe(browserSync.stream());
 }
 
+// watch changes and reload browser
 function watch() {
   browserSync.init({
     server: {
@@ -20,5 +24,19 @@ function watch() {
   gulp.watch('*.html').on('change', browserSync.reload);
 }
 
+//Distribution task
+function dist() {
+  var plugins = [
+        autoprefixer(),
+        cssnano({preset: 'default'})
+    ];
+    return gulp.src('./src/css/*.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+
+
 exports.style = style;
 exports.watch = watch;
+exports.dist = dist;
